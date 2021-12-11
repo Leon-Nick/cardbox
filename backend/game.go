893457fa91d222db2ef69709c8c7414ea2319d1a
LeonBaram <leonbaram@protonmail.com>
2021-12-11 18:45:46 -0500

    implement GameHandle methods

--- backend/game.go
+++ backend/game.go
@@ -1,6 +1,9 @@
 package backend
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Game struct {
 	// GameState
@@ -8,4 +11,8 @@ type Game struct {
 	ID             string    `json:"id"`
 	CreatedAt      time.Time `json:"created_at"`
 	UpdatedAt      time.Time `json:"updated_at"`
+}
+
+func (g *Game) StateID() string {
+	return fmt.Sprintf("%019d", g.UpdatedAt.UnixNano())
 }

c7b65e3eb5e12872ac62f2dbe01ede2c6a047268
LeonBaram <leonbaram@protonmail.com>
2021-12-12 03:10:10 -0500

    Implement basic post demo

--- backend/index.ts
+++ backend/index.ts
@@ -1,18 +1,39 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { Game } from "./models/game";
 
 const server = express();
 
+server.use(bodyParser.json());
+
 const sessions: Record<string, Game> = {};
 
+type ReqQuery = { id: string };
+type ReqBody = {
+  meme: boolean;
+};
+
 server.get("/session", (req, res) => {
-  const { id } = req.params as { id: string };
+  const { id } = req.query as ReqQuery;
   if (!(id in sessions)) {
     sessions[id] = {
       meme: false,
     };
   }
   res.send(sessions[id]);
+  console.log(sessions);
+});
+
+server.post("/session", (req, res) => {
+  const { id } = req.query as ReqQuery;
+  const { meme } = req.body as ReqBody;
+  if (id in sessions) {
+    sessions[id].meme = meme;
+    res.send(true);
+  } else {
+    res.send(false);
+  }
+  console.log(sessions);
 });
 
 server.listen(5000);
--- frontend/pages/games/[id].tsx
+++ frontend/pages/games/[id].tsx
@@ -2,27 +2,54 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { Game } from "../../models/game";
 
+type Update = {
+  meme: boolean;
+};
+
 async function fetchGameState(id: string): Promise<Game> {
   const res = await fetch(`http://localhost:5000/session?id=${id}`);
   const data: Game = await res.json();
   return data;
 }
 
+async function updateGameState(id: string, update: Update): Promise<boolean> {
+  const res = await fetch(`http://localhost:5000/session?id=${id}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(update),
+  });
+
+  const success: boolean = await res.json();
+  return success;
+}
+
 const GamePage: React.FC = () => {
   const router = useRouter();
+  const id = router.query.id as string;
   const [gameState, setGameState] = useState<Game>();
 
   useEffect(() => {
-    const id = router.query.id as string;
     fetchGameState(id).then(setGameState);
-  }, [router.query]);
+  }, [id]);
 
   console.log(gameState);
   return (
     <div>
-      <button onClick={() => null}>
-        {gameState && <h1>{gameState.meme ? "a" : "b"}</h1>}
-      </button>
+      {gameState && (
+        <button
+          onClick={() =>
+            console.log(
+              updateGameState(id, {
+                meme: !gameState.meme,
+              })
+            )
+          }
+        >
+          <h1>{gameState.meme ? "a" : "b"}</h1>
+        </button>
+      )}
     </div>
   );
 };

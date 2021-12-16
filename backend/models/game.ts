5a3e922c4ae024d2965af8e2ee0aa98eeb888d6c
LeonBaram <leonbaram@protonmail.com>
2021-12-15 19:00:34 -0500

    Upgrade "Game" from interface to class

--- backend/models/game.ts
+++ backend/models/game.ts
@@ -1,3 +1,13 @@
-export interface Game {
-  meme: boolean;
+export class Game {
+  public ID: string;
+  public players: Set<string>;
+  public hostID: string;
+  public meme: boolean;
+
+  constructor(roomID: string, hostID: string) {
+    this.ID = roomID;
+    this.players = new Set();
+    this.hostID = hostID;
+    this.meme = false;
+  }
 }
--- frontend/models/game.ts
+++ frontend/models/game.ts
@@ -1,3 +1,13 @@
-export interface Game {
-  meme: boolean;
+export class Game {
+  public ID: string;
+  public players: Set<string>;
+  public hostID: string;
+  public meme: boolean;
+
+  constructor(roomID: string, hostID: string) {
+    this.ID = roomID;
+    this.hostID = hostID;
+    this.players = new Set(hostID);
+    this.meme = false;
+  }
 }

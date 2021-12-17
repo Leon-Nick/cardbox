482c20d11dffa06993e45dcfceb2afe44749219d
LeonBaram <leonbaram@protonmail.com>
2021-12-17 03:58:40 -0500

    Added and reorganized events

--- backend/common/events.ts
+++ backend/common/events.ts
@@ -1,6 +1,37 @@
 export enum Event {
+  // Socket.io Events
   Connection = "connection",
-  JoinedRoom = "joinedRoom",
-  UpdatedGameState = "updatedGameState",
   Disconnecting = "disconnecting",
+
+  // Out-Of-Game Events
+  RoomCreated = "RoomCreated",
+  RoomDeleted = "RoomDeleted",
+  RoomUnlocked = "RoomUnlocked",
+  RoomLocked = "RoomLocked",
+  PlayerJoined = "PlayerJoined",
+  PlayerLeft = "PlayerLeft",
+  HostChanged = "HostChanged",
+
+  // In-Game Events: Card
+  CardCreated = "CardCreated",
+  CardDeleted = "CardDeleted",
+  CardMoved = "CardMoved",
+  CardRotated = "CardRotated",
+
+  // In-Game Events: Counter
+  CounterCreated = "CounterCreated",
+  CounterDeleted = "CounterDeleted",
+  CounterValAdded = "CounterValAdded",
+  CounterValRemoved = "CounterValRemoved",
+  CounterValChanged = "CounterValChanged",
+  CounterMoved = "CounterMoved",
+  CounterRotated = "CounterRotated",
+
+  // In-Game Events: CardStack
+  CardStackCreated = "CardStackCreated",
+  CardStackDeleted = "CardStackDeleted",
+  CardStackShuffled = "CardStackShuffled",
+  CardStackModified = "CardStackModified",
+  CardStackMoved = "CardStackMoved",
+  CardStackRotated = "CardStackRotated",
 }

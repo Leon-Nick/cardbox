cd3891cba1b692c448960609b755d720a9a71d2d
LeonBaram <leonbaram@protonmail.com>
2021-12-17 16:49:03 -0500

    Switch model constructors to "initArgs" pattern
    Each class in common/models now comes with a "_InitArgs" interface,
    which defines the arguments that should be passed to its constructor.
    This streamlines and organizes object initialization, with the added
    bonus of not having to remember in what order to pass the initializing
    arguments.

--- backend/common/models/Card.ts
+++ backend/common/models/Card.ts
@@ -1,31 +1,27 @@
-import { randomUUID } from "crypto";
 import { Physical } from "./Physical";
 import { ScryfallData } from "./ScryfallData";
 
-export class Card implements Physical {
-  public ID: string;
-  public data: ScryfallData;
+export interface CardInitArgs {
+  ID: string;
+  data: ScryfallData;
+  height: string;
+  width: string;
+  x: number;
+  y: number;
+}
 
-  public height: string;
-  public width: string;
-  public x: number;
-  public y: number;
+export class Card implements Physical {
+  public ID!: string;
+  public data!: ScryfallData;
+
+  public height!: string;
+  public width!: string;
+  public x!: number;
+  public y!: number;
   public rotation: number;
 
-  constructor(
-    data: ScryfallData,
-    height: string,
-    width: string,
-    x: number,
-    y: number
-  ) {
-    this.ID = randomUUID();
-    this.data = data;
-
-    this.height = height;
-    this.width = width;
-    this.x = x;
-    this.y = y;
+  constructor(args: CardInitArgs) {
+    Object.assign(this, args);
     this.rotation = 0;
   }
 }

641c329696e15c7599be254ce79f9514dfcfa1ac
LeonBaram <leonbaram@protonmail.com>
2021-12-30 18:45:44 -0500

    Refactor game object models
    - change interface GameObject to be superclass instead
    - implement common sprite interaction operations in
      GameObject.initSprite()

--- server/shared/models/GameObjects.ts
+++ server/shared/models/GameObjects.ts
@@ -1,59 +1,92 @@
 import { randomUUID } from "crypto";
-import { Sprite } from "pixi.js";
+import { Container, InteractionEvent, Sprite } from "pixi.js";
 import { ScryfallData } from "./ScryfallData";
 import { assets } from "../assets/manifest";
 
-export interface GameObject {
-  type: "Card" | "CardStack" | "Counter";
-  ID: string;
-  sprite: Sprite;
+export type GameObjectSubtype = "Card" | "CardStack" | "Counter";
+
+export interface GameObjectInitArgs {
+  ID?: string;
+  type: GameObjectSubtype;
+  imageURL: string;
 }
 
-export class Card implements GameObject {
-  type: "Card" = "Card";
+export class GameObject {
+  type: GameObjectSubtype;
   ID: string;
 
+  sprite: Sprite;
+  event?: InteractionEvent;
+
+  constructor(args: GameObjectInitArgs) {
+    this.ID = args.ID ?? randomUUID();
+    this.type = args.type;
+    this.sprite = Sprite.from(args.imageURL);
+  }
+
+  initSprite(stage: Container) {
+    const { sprite } = this;
+    sprite.scale.set(0.3);
+    sprite.anchor.set(0.5);
+    sprite.interactive = true;
+    sprite
+      .on("pointerdown", (event: InteractionEvent) => {
+        this.event = event;
+      })
+      .on("pointermove", () => {
+        if (this.event?.data) {
+          const { x, y } = this.event.data.getLocalPosition(stage);
+          sprite.x = x;
+          sprite.y = y;
+        }
+      })
+      .on("pointerup", () => {
+        this.event = undefined;
+      })
+      .on("pointerupoutside", () => {
+        this.event = undefined;
+      });
+  }
+}
+
+export class Card extends GameObject {
   // in-game info (name, image URL, oracle text, etc)
   data: ScryfallData;
 
-  // display info (position, rotation, scale, etc)
-  sprite: Sprite;
-
   constructor(data: ScryfallData, ID?: string) {
-    this.ID = ID ?? randomUUID();
+    super({
+      imageURL: data.image_uris?.normal ?? assets.island,
+      type: "Card",
+      ID,
+    });
+
     // TODO: impl. card data import from scryfall
     this.data = data;
-    this.sprite = Sprite.from(data.image_uris?.normal ?? assets.island);
   }
 }
 
-export class CardStack implements GameObject {
-  type: "CardStack" = "CardStack";
-  ID: string;
-
+export class CardStack extends GameObject {
   // in-game info (names, image URLs, oracle text, etc)
   cards: ScryfallData[];
 
-  // display info (position, rotation, scale, etc)
-  sprite: Sprite;
-
   constructor(cards: ScryfallData[], ID?: string) {
-    this.ID = ID ?? randomUUID();
+    super({
+      imageURL: cards[0].image_uris?.normal ?? assets.island,
+      type: "CardStack",
+      ID,
+    });
+
     this.cards = cards;
-    this.sprite = Sprite.from(cards[0].image_uris?.normal ?? assets.island);
   }
 }
 
-export class Counter implements GameObject {
-  type: "Counter" = "Counter";
-  ID: string;
-
-  // display info (position, rotation, scale, etc)
-  sprite: Sprite;
-
+export class Counter extends GameObject {
   constructor(ID?: string) {
-    this.ID = ID ?? randomUUID();
-    this.sprite = Sprite.from(assets.burgeoning);
+    super({
+      imageURL: assets.burgeoning,
+      type: "Counter",
+      ID,
+    });
   }
 
   get val() {
@@ -62,5 +95,6 @@ export class Counter implements GameObject {
 
   set val(n: number) {
     // TODO: impl. set val by modifying this.sprite
+    n = n;
   }
 }

f0e738f1b0a0c8f91daa7600191af02c4c12cd81
LeonBaram <leonbaram@protonmail.com>
2022-01-02 18:16:40 -0500

    Update models for Game and GameObjects
    In Game.ts:
    - change property 'players' from a set of player ID strings, to a map
      where keys are player ID strings and values are Player objects
    - remove property 'locked'
    - merge properties 'cards', 'cardStacks', 'counters' into one property,
      'gameObjects'
    - remove toJSON method and gameStr function
    
    In GameObjects.ts:
    - rename CardStack to Deck
    - rename GameObjectInitArgs to SuperInitArgs and remove export
    - add x and y coordinates to SuperInitArgs and GameObject constructor

--- server/shared/models/Game.ts
+++ server/shared/models/Game.ts
@@ -1,6 +1,5 @@
-import { Card } from "./Card";
-import { Counter } from "./Counter";
-import { CardStack } from "./CardStack";
+import { Player } from ".";
+import { GameObject } from "./GameObjects";
 
 export interface GameInitArgs {
   roomID: string;
@@ -9,41 +8,17 @@ export interface GameInitArgs {
 
 export class Game {
   public ID: string;
-  public players: Set<string>;
   public hostID: string;
-  public locked: boolean;
 
-  public cards: Record<string, Card>;
-  public cardStacks: Record<string, CardStack>;
-  public counters: Record<string, Counter>;
+  public players: Record<string, Player>;
+  public gameObjects: Record<string, GameObject>;
 
   constructor(args: GameInitArgs) {
     const { roomID, hostID } = args;
     this.ID = roomID;
-    this.players = new Set();
-    this.players.add(hostID);
     this.hostID = hostID;
-    this.locked = false;
-
-    this.cards = {};
-    this.counters = {};
-    this.cardStacks = {};
+    const host = new Player({ roomID, ID: hostID });
+    this.players = { hostID: host };
+    this.gameObjects = {};
   }
-
-  toJSON() {
-    const json = { ...this, players: Array.from(this.players) };
-    return JSON.stringify(json);
-  }
-}
-
-export function gameStr(game: Game): string {
-  return (
-    "{\n\t" +
-    Object.entries(game)
-      .map(
-        ([key, val]) => `${key}: ${key === "players" ? Array.from(val) : val}`
-      )
-      .join("\n\t") +
-    "\n}"
-  );
 }
